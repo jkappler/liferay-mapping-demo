1a3a3d8572d4ea4fc420ccb72e10d27c5dc9281e
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-09-29 19:54:37 +0200

    Base 05: Adds resource actions and permission

---
+++ modules/starship/starship-service/src/main/java/com/liferay/starship/internal/security/permission/resource/StarshipPortletResourcePermissionRegistrar.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.starship.internal.security.permission.resource;
+
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.starship.constants.StarshipConstants;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = {})
+public class StarshipPortletResourcePermissionRegistrar {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("resource.name", StarshipConstants.RESOURCE_NAME);
+
+		_serviceRegistration = bundleContext.registerService(
+			PortletResourcePermission.class,
+			PortletResourcePermissionFactory.create(
+				StarshipConstants.RESOURCE_NAME),
+			properties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	private ServiceRegistration<PortletResourcePermission> _serviceRegistration;
+
+}
