ee56bf4ed3bea5fab544f50150136cf35711b365
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-09-29 20:08:14 +0200

    Step 06: Adds CRUD methods for starship entity

--- modules/starship/starship-service/src/main/java/com/liferay/starship/service/impl/StarshipEntryLocalServiceImpl.java
+++ modules/starship/starship-service/src/main/java/com/liferay/starship/service/impl/StarshipEntryLocalServiceImpl.java
@@ -15,22 +15,31 @@
 package com.liferay.starship.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.starship.exception.DuplicateStarshipEntryNameException;
+import com.liferay.starship.exception.StarshipEntryNameException;
+import com.liferay.starship.model.StarshipEntry;
 import com.liferay.starship.service.base.StarshipEntryLocalServiceBaseImpl;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The implementation of the starship entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.starship.service.StarshipEntryLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see StarshipEntryLocalServiceBaseImpl
+ * @author Jürgen Kappler
  */
 @Component(
 	property = "model.class.name=com.liferay.starship.model.StarshipEntry",
@@ -39,10 +48,158 @@ import org.osgi.service.component.annotations.Component;
 public class StarshipEntryLocalServiceImpl
 	extends StarshipEntryLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.starship.service.StarshipEntryLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.starship.service.StarshipEntryLocalServiceUtil</code>.
-	 */
+	@Override
+	public StarshipEntry addStarshipEntry(
+			long userId, long groupId, String name, String description,
+			File starshipFileImage, int status, ServiceContext serviceContext)
+		throws PortalException {
+
+		_validateName(groupId, name);
+
+		User user = userLocalService.getUser(userId);
+
+		long starshipEntryId = counterLocalService.increment();
+
+		StarshipEntry starshipEntry = starshipEntryPersistence.create(
+			starshipEntryId);
+
+		starshipEntry.setUuid(serviceContext.getUuid());
+		starshipEntry.setGroupId(groupId);
+		starshipEntry.setCompanyId(user.getCompanyId());
+		starshipEntry.setUserId(user.getUserId());
+		starshipEntry.setUserName(user.getFullName());
+		starshipEntry.setCreateDate(serviceContext.getCreateDate(new Date()));
+		starshipEntry.setModifiedDate(
+			serviceContext.getModifiedDate(new Date()));
+		starshipEntry.setName(name);
+		starshipEntry.setDescription(description);
+		starshipEntry.setStarshipFileImageId(counterLocalService.increment());
+		starshipEntry.setStatus(status);
+
+		starshipEntry = starshipEntryPersistence.update(starshipEntry);
+
+		// Image
+
+		if (starshipFileImage != null) {
+			byte[] starhipImageBytes = null;
+
+			try {
+				starhipImageBytes = FileUtil.getBytes(starshipFileImage);
+
+				if (starhipImageBytes != null) {
+					imageLocalService.updateImage(
+						starshipEntry.getStarshipFileImageId(),
+						starhipImageBytes);
+				}
+			}
+			catch (IOException ioException) {
+			}
+		}
+
+		// Resources
+
+		resourceLocalService.addResources(
+			starshipEntry.getCompanyId(), starshipEntry.getGroupId(),
+			starshipEntry.getUserId(), StarshipEntry.class.getName(),
+			starshipEntry.getPrimaryKey(), false, true, true);
+
+		return starshipEntry;
+	}
+
+	@Override
+	public StarshipEntry deleteStarshipEntry(long starshipEntryId)
+		throws PortalException {
+
+		return deleteStarshipEntry(getStarshipEntry(starshipEntryId));
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public StarshipEntry deleteStarshipEntry(StarshipEntry starshipEntry)
+		throws PortalException {
+
+		starshipEntryPersistence.remove(starshipEntry);
+
+		resourceLocalService.deleteResource(
+			starshipEntry, ResourceConstants.SCOPE_INDIVIDUAL);
+
+		return starshipEntry;
+	}
+
+	@Override
+	public List<StarshipEntry> getStarshipEntries(long groupId) {
+		return starshipEntryPersistence.findByGroupId(groupId);
+	}
+
+	@Override
+	public StarshipEntry getStarshipEntry(long groupId, String name)
+		throws PortalException {
+
+		return starshipEntryPersistence.findByG_N(groupId, name);
+	}
+
+	@Override
+	public StarshipEntry updateStarshipEntry(
+			long starshipEntryId, String name, String description,
+			File starshipFileImage, int status)
+		throws PortalException {
+
+		StarshipEntry starshipEntry = starshipEntryPersistence.findByPrimaryKey(
+			starshipEntryId);
+
+		if (!Objects.equals(starshipEntry.getName(), name)) {
+			_validateName(starshipEntry.getGroupId(), name);
+		}
+
+		starshipEntry.setModifiedDate(new Date());
+		starshipEntry.setName(name);
+		starshipEntry.setDescription(description);
+		starshipEntry.setStatus(status);
+
+		starshipEntry = starshipEntryPersistence.update(starshipEntry);
+
+		// Image
+
+		if (starshipFileImage != null) {
+			byte[] starhipImageBytes = null;
+
+			try {
+				starhipImageBytes = FileUtil.getBytes(starshipFileImage);
+
+				if (starhipImageBytes != null) {
+					imageLocalService.updateImage(
+						starshipEntry.getStarshipFileImageId(),
+						starhipImageBytes);
+				}
+			}
+			catch (IOException ioException) {
+			}
+		}
+
+		return starshipEntry;
+	}
+
+	private void _validateName(long groupId, String name)
+		throws PortalException {
+
+		if (Validator.isNull(name)) {
+			throw new StarshipEntryNameException("Name is null");
+		}
+
+		int nameMaxLength = ModelHintsUtil.getMaxLength(
+			StarshipEntry.class.getName(), "name");
+
+		if (name.length() > nameMaxLength) {
+			throw new StarshipEntryNameException(
+				"Name has more than " + nameMaxLength + " characters");
+		}
+
+		StarshipEntry starship = starshipEntryPersistence.fetchByG_N(
+			groupId, name);
+
+		if (starship != null) {
+			throw new DuplicateStarshipEntryNameException();
+		}
+	}
 
 }
