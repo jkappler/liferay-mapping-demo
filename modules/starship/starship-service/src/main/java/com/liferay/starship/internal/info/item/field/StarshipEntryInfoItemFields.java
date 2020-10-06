6252a3380ab92f0a15b6de43b22b1326807e86fa
Pablo Molina <contact@pablomolina.me>
2020-10-06 12:52:33 +0200

    Step 16: Add empty StarshipEntryInfoItemFields

---
+++ modules/starship/starship-service/src/main/java/com/liferay/starship/internal/info/item/field/StarshipEntryInfoItemFields.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.starship.internal.info.item.field;
+
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.localized.InfoLocalizedValue;
+
+/**
+ * @author Jürgen Kappler
+ * @author Pablo Molina
+ * @author Verónica González
+ */
+public interface StarshipEntryInfoItemFields {
+
+	public static final InfoField<TextInfoFieldType> nameInfoField =
+		InfoField.builder(
+		).infoFieldType(
+			TextInfoFieldType.INSTANCE
+		).name(
+			"name"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(
+				StarshipEntryInfoItemFields.class, "name")
+		).build();
+
+}
