b10ddb110ca63d472e3b942af8c62cff1dcd5622
Pablo Molina <contact@pablomolina.me>
2020-10-06 11:16:39 +0200

    Workshop 03: Add empty StarshipEntryItemSelectorView

---
+++ modules/starship/starship-web/src/main/java/com/liferay/starship/web/internal/item/selector/StarshipEntryItemSelectorView.java
@@ -0,0 +1,189 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.starship.web.internal.item.selector;
+
+import com.liferay.info.item.selector.InfoItemSelectorView;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
+import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
+import com.liferay.item.selector.criteria.InfoItemItemSelectorReturnType;
+import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.starship.model.StarshipEntry;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = "item.selector.view.order:Integer=200",
+	service = ItemSelectorView.class
+)
+public class StarshipEntryItemSelectorView
+	implements InfoItemSelectorView,
+			   ItemSelectorView<InfoItemItemSelectorCriterion> {
+
+	@Override
+	public String getClassName() {
+		return StarshipEntry.class.getName();
+	}
+
+	@Override
+	public Class<InfoItemItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return InfoItemItemSelectorCriterion.class;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			InfoItemItemSelectorCriterion infoItemItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		_itemSelectorViewDescriptorRenderer.renderHTML(
+			servletRequest, servletResponse, infoItemItemSelectorCriterion,
+			portletURL, itemSelectedEventName, search,
+			new StarshipItemSelectorViewDescriptor(
+				(HttpServletRequest)servletRequest, portletURL));
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.singletonList(
+			new InfoItemItemSelectorReturnType());
+
+	@Reference
+	private ItemSelectorViewDescriptorRenderer<InfoItemItemSelectorCriterion>
+		_itemSelectorViewDescriptorRenderer;
+
+	private class StarshipEntryItemDescriptor
+		implements ItemSelectorViewDescriptor.ItemDescriptor {
+
+		public StarshipEntryItemDescriptor(
+			StarshipEntry starshipEntry,
+			HttpServletRequest httpServletRequest) {
+
+			_starshipEntry = starshipEntry;
+
+			_httpServletRequest = httpServletRequest;
+
+			_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+		}
+
+		@Override
+		public String getIcon() {
+			return null;
+		}
+
+		@Override
+		public String getImageURL() {
+			return null;
+		}
+
+		@Override
+		public String getPayload() {
+			return null;
+		}
+
+		@Override
+		public String getSubtitle(Locale locale) {
+			return null;
+		}
+
+		@Override
+		public String getTitle(Locale locale) {
+			return null;
+		}
+
+		private HttpServletRequest _httpServletRequest;
+		private final StarshipEntry _starshipEntry;
+		private final ThemeDisplay _themeDisplay;
+
+	}
+
+	private class StarshipItemSelectorViewDescriptor
+		implements ItemSelectorViewDescriptor<StarshipEntry> {
+
+		public StarshipItemSelectorViewDescriptor(
+			HttpServletRequest httpServletRequest, PortletURL portletURL) {
+
+			_httpServletRequest = httpServletRequest;
+			_portletURL = portletURL;
+
+			_portletRequest = (PortletRequest)_httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
+
+			_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+		}
+
+		@Override
+		public ItemDescriptor getItemDescriptor(StarshipEntry starshipEntry) {
+			return null;
+		}
+
+		@Override
+		public ItemSelectorReturnType getItemSelectorReturnType() {
+			return null;
+		}
+
+		@Override
+		public SearchContainer<StarshipEntry> getSearchContainer()
+			throws PortalException {
+
+			return null;
+		}
+
+		private HttpServletRequest _httpServletRequest;
+		private final PortletRequest _portletRequest;
+		private final PortletURL _portletURL;
+		private final ThemeDisplay _themeDisplay;
+
+	}
+
+}
