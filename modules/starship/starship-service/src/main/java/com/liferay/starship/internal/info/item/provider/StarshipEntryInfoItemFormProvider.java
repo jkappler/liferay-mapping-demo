6d3d9d7790026ba5b8d4a4d0cb0fbaa67b337ba6
Pablo Molina <contact@pablomolina.me>
2020-10-06 13:00:13 +0200

    Step 17: Add empty info item related classes

---
+++ modules/starship/starship-service/src/main/java/com/liferay/starship/internal/info/item/provider/StarshipEntryInfoItemFormProvider.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.starship.internal.info.item.provider;
+
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.starship.model.StarshipEntry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Verónica González
+ * @author Pablo Molina
+ * @author Jürgen Kappler
+ */
+@Component(service = InfoItemFormProvider.class)
+public class StarshipEntryInfoItemFormProvider
+	implements InfoItemFormProvider<StarshipEntry> {
+
+	@Override
+	public InfoForm getInfoForm() {
+		return null;
+	}
+
+}
