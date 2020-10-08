ec84bdb24cb4cd119981f4777b8025f9d072d830
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-10-08 20:20:07 +0200

    Step 08 - Adds Friendly URL resolver, so that when someone navigates to an url with /s/[id of the starship] it will land on the corresponding display page

---
+++ modules/starship/starship-service/src/main/java/com/liferay/starship/internal/layout/display/page/StarshipEntryDisplayPageFriendlyURLResolver.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.starship.internal.layout.display.page;
+
+import com.liferay.asset.display.page.portlet.BaseAssetDisplayPageFriendlyURLResolver;
+import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = FriendlyURLResolver.class)
+public class StarshipEntryDisplayPageFriendlyURLResolver
+	extends BaseAssetDisplayPageFriendlyURLResolver {
+
+	@Override
+	public String getURLSeparator() {
+		return "/s/";
+	}
+
+}
